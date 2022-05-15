e10fde6e7c343127f501ddd462c5dddfb61ea3aa
99 <hahahoho5915@gmail.com>
2022-05-15 19:25:15 +0900

    level1_없는 숫자 더하기
    url: https://programmers.co.kr/learn/courses/30/lessons/86051

---
+++ src/level1/EmptyNumberSum.java
@@ -0,0 +1,21 @@
+package level1;
+
+import java.util.Arrays;
+
+/*
+programmers
+level: 1
+title: 없는숫자 더하기
+url: https://programmers.co.kr/learn/courses/30/lessons/86051
+*/
+public class EmptyNumberSum {
+	public int solution(int[] numbers) {
+		return 45 - Arrays.stream(numbers).sum();
+	}
+
+	public static void main(String[] args) {
+		int[] numbers = {1, 2, 3, 4, 6, 7, 8, 0};
+		EmptyNumberSum emptyNumberSum = new EmptyNumberSum();
+		System.out.println(emptyNumberSum.solution(numbers));
+	}
+}
