1dc56ad2502a585fc41708d66f3c9dbb03586f25
99 <hahahoho5915@gmail.com>
2022-05-15 21:03:52 +0900

    level1_음양 더하기
    url: https://programmers.co.kr/learn/courses/30/lessons/76501

---
+++ src/level1/SumPlusMinus.java
@@ -0,0 +1,27 @@
+package level1;
+
+/*
+programmers
+level: 1
+title: 음양 더하기
+url: https://programmers.co.kr/learn/courses/30/lessons/76501
+*/
+public class SumPlusMinus {
+	public int solution(int[] absolutes, boolean[] signs) {
+
+		int answer = 0;
+		for (int i = 0; i < absolutes.length; i++) {
+			answer += absolutes[i] * (signs[i] ? 1 : -1);
+		}
+
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		int[] absolutes = {4, 7, 12};
+		boolean[] signs = {true, false, true};
+		SumPlusMinus sumPlusMinus = new SumPlusMinus();
+		int solution = sumPlusMinus.solution(absolutes, signs);
+		System.out.println("solution = " + solution);
+	}
+}
