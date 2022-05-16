4fd214949e632a1dbeaf1caf014c8774e9e2e6bc
99 <hahahoho5915@gmail.com>
2022-05-16 18:12:31 +0900

    level1_로또의 최고 순위와 최저 순위
    url: https://programmers.co.kr/learn/courses/30/lessons/77484

---
+++ src/level1/LottoRank.java
@@ -0,0 +1,41 @@
+package level1;
+
+import java.util.Arrays;
+
+/*
+programmers
+level: 1
+title: 로또의 최고 순위와 최저 순위
+url: https://programmers.co.kr/learn/courses/30/lessons/77484
+*/
+public class LottoRank {
+	public int[] solution(int[] lottos, int[] win_nums) {
+		int[] result = {6, 6, 5, 4, 3, 2, 1};
+		int sameCount = 0;
+		int zeroCount = 0;
+		for (int i = 0; i < 6; i++) {
+			if (lottos[i] == 0) {
+				zeroCount++;
+				continue;
+			}
+			for (int j = 0; j < 6; j++) {
+				if (lottos[i] == win_nums[j]) {
+					sameCount++;
+				}
+			}
+		}
+
+		int[] answer = new int[2];
+		answer[0] = result[sameCount + zeroCount];
+		answer[1] = result[sameCount];
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		int[] lottos = {44, 1, 0, 0, 31, 25};
+		int[] win_nums = {31, 10, 45, 1, 6, 19};
+		LottoRank lottoRank = new LottoRank();
+		int[] solution = lottoRank.solution(lottos, win_nums);
+		System.out.println("solution = " + Arrays.toString(solution));
+	}
+}
