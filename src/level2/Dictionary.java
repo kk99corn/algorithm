762bf74f6d8a95072cf328256efc991b0a8be9a4
99 <hahahoho5915@gmail.com>
2022-05-23 19:50:48 +0900

    level2_모음사전(재귀함수로 변경)
    url: https://programmers.co.kr/learn/courses/30/lessons/84512

--- src/level2/Dictionary.java
+++ src/level2/Dictionary.java
@@ -12,35 +12,50 @@ url: https://programmers.co.kr/learn/courses/30/lessons/84512
 public class Dictionary {
 	static ArrayList<String> wordList = new ArrayList<>();
 
-	// 최초풀이
-	public int solution(String word) {
-		String[] words = {"A", "E", "I", "O", "U"};
+	public void r (String sumStr, int n) {
+		if (n > 5) return;
 
+		wordList.add(sumStr);
 		for (int i = 0; i < 5; i++) {
-			wordList.add(words[i]);
-			for (int j = 0; j < 5; j++) {
-				wordList.add(words[i] + words[j]);
-				for (int k = 0; k < 5; k++) {
-					wordList.add(words[i] + words[j] + words[k]);
-					for (int l = 0; l < 5; l++) {
-						wordList.add(words[i] + words[j] + words[k] + words[l]);
-						for (int n = 0; n < 5; n++) {
-							wordList.add(words[i] + words[j] + words[k] + words[l] + words[n]);
-						}
-					}
-				}
-			}
+			r(sumStr + "AEIOU".charAt(i), n + 1);
 		}
-		Collections.sort(wordList);
-		int answer = 1;
-		for (String s : wordList) {
-			if (s.equals(word)) {
-				break;
-			}
-			answer++;
-		}
-		return answer;
 	}
+
+	public int solution(String word) {
+		r("", 0);
+		return wordList.indexOf(word);
+	}
+//	static ArrayList<String> wordList = new ArrayList<>();
+//
+//	// 최초풀이
+//	public int solution(String word) {
+//		String[] words = {"A", "E", "I", "O", "U"};
+//
+//		for (int i = 0; i < 5; i++) {
+//			wordList.add(words[i]);
+//			for (int j = 0; j < 5; j++) {
+//				wordList.add(words[i] + words[j]);
+//				for (int k = 0; k < 5; k++) {
+//					wordList.add(words[i] + words[j] + words[k]);
+//					for (int l = 0; l < 5; l++) {
+//						wordList.add(words[i] + words[j] + words[k] + words[l]);
+//						for (int n = 0; n < 5; n++) {
+//							wordList.add(words[i] + words[j] + words[k] + words[l] + words[n]);
+//						}
+//					}
+//				}
+//			}
+//		}
+//		Collections.sort(wordList);
+//		int answer = 1;
+//		for (String s : wordList) {
+//			if (s.equals(word)) {
+//				break;
+//			}
+//			answer++;
+//		}
+//		return answer;
+//	}
 
 	public static void main(String[] args) {
 		Dictionary dictionary = new Dictionary();
