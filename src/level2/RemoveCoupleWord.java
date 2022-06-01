c21c189449c10c703b1e6999cc2ec0d3e3053b88
99 <hahahoho5915@gmail.com>
2022-06-01 18:34:35 +0900

    level2_짝지어 제거하기
    url: https://programmers.co.kr/learn/courses/30/lessons/12973

---
+++ src/level2/RemoveCoupleWord.java
@@ -0,0 +1,39 @@
+package level2;
+
+import java.util.Stack;
+
+/*
+programmers
+level: 2
+title: 짝지어 제거하기
+url: https://programmers.co.kr/learn/courses/30/lessons/12973
+*/
+public class RemoveCoupleWord {
+
+	public int solution(String s) {
+		Stack<Character> stack = new Stack<>();
+		for (int i = 0; i < s.length(); i++) {
+			if (stack.isEmpty()) {
+				stack.push(s.charAt(i));
+			} else {
+				Character c = stack.peek();
+				if (c == s.charAt(i)) {
+					stack.pop();
+				} else {
+					stack.push(s.charAt(i));
+				}
+			}
+		}
+		if (stack.size() > 0) {
+			return 0;
+		} else {
+			return 1;
+		}
+	}
+
+	public static void main(String[] args) {
+		RemoveCoupleWord removeCoupleWord = new RemoveCoupleWord();
+		int baabaa = removeCoupleWord.solution("baabaa");
+		System.out.println("baabaa = " + baabaa);
+	}
+}
