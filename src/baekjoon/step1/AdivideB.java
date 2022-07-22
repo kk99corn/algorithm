589bf86ba50fded74196aadf6bf9d61303c134a6
99 <hahahoho5915@gmail.com>
2022-07-22 20:27:08 +0900

    step1_A/B
    url: https://www.acmicpc.net/problem/1008

---
+++ src/baekjoon/step1/AdivideB.java
@@ -0,0 +1,19 @@
+package baekjoon.step1;
+
+import java.util.Scanner;
+
+/*
+baekjoon
+step: 1
+title: A/B
+url: https://www.acmicpc.net/problem/1008
+*/
+public class AdivideB {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		double a = scanner.nextDouble();
+		double b = scanner.nextDouble();
+
+		System.out.println(a / b);
+	}
+}
