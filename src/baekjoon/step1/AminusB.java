5c4b3f3c5bb8dbbc7b84140470f350f71dcc0474
99 <hahahoho5915@gmail.com>
2022-07-22 20:21:24 +0900

    step1_A-B
    url: https://www.acmicpc.net/problem/1001

---
+++ src/baekjoon/step1/AminusB.java
@@ -0,0 +1,19 @@
+package baekjoon.step1;
+
+import java.util.Scanner;
+
+/*
+baekjoon
+step: 1
+title: A-B
+url: https://www.acmicpc.net/problem/1001
+*/
+public class AminusB {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+
+		System.out.println(a - b);
+	}
+}
