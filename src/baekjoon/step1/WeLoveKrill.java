8769911915cccc70f6d978700330be9778944f09
99 <hahahoho5915@gmail.com>
2022-07-22 20:09:18 +0900

    step1_크릴
    url: https://www.acmicpc.net/problem/10718

---
+++ src/baekjoon/step1/WeLoveKrill.java
@@ -0,0 +1,14 @@
+package baekjoon.step1;
+
+/*
+baekjoon
+step: 1
+title: We love kriii
+url: https://www.acmicpc.net/problem/10718
+*/
+public class WeLoveKrill {
+	public static void main(String[] args) {
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+	}
+}
