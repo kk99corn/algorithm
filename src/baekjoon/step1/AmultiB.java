435e0189bb20c977ff9ae054c002cc055c7ac23c
99 <hahahoho5915@gmail.com>
2022-07-22 20:26:57 +0900

    step1_AXB
    url: https://www.acmicpc.net/problem/10998

---
+++ src/baekjoon/step1/AmultiB.java
@@ -0,0 +1,19 @@
+package baekjoon.step1;
+
+import java.util.Scanner;
+
+/*
+baekjoon
+step: 1
+title: AXB
+url: https://www.acmicpc.net/problem/10998
+*/
+public class AmultiB {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+
+		System.out.println(a * b);
+	}
+}
