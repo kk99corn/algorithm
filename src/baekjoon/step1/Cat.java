f00e8171897d9b346db8c4c2562f6cb91047fc17
99 <hahahoho5915@gmail.com>
2022-07-22 20:09:28 +0900

    step1_고양이
    url: https://www.acmicpc.net/problem/10171

---
+++ src/baekjoon/step1/Cat.java
@@ -0,0 +1,13 @@
+package baekjoon.step1;
+
+/*
+baekjoon
+step: 1
+title: 고양이
+url: https://www.acmicpc.net/problem/10171
+*/
+public class Cat {
+	public static void main(String[] args) {
+		System.out.println("\\    /\\\n" + " )  ( ')\n" + "(  /  )\n" + " \\(__)|\n");
+	}
+}
