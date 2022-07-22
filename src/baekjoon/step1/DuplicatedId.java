ac0865b4e8b890579b440be860434ada7f89bef5
99 <hahahoho5915@gmail.com>
2022-07-22 20:30:31 +0900

    step1_??!
    url: https://www.acmicpc.net/problem/10926

---
+++ src/baekjoon/step1/DuplicatedId.java
@@ -0,0 +1,18 @@
+package baekjoon.step1;
+
+import java.util.Scanner;
+
+/*
+baekjoon
+step: 1
+title: ??!
+url: https://www.acmicpc.net/problem/10926
+*/
+public class DuplicatedId {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String id = scanner.next();
+
+		System.out.println(id + "??!");
+	}
+}
