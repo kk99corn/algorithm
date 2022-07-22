6a1002ed389c3e17fa1080bbbb515b63773150cb
99 <hahahoho5915@gmail.com>
2022-07-22 20:21:01 +0900

    step1_개
    url: https://www.acmicpc.net/problem/10172

---
+++ src/baekjoon/step1/Dog.java
@@ -0,0 +1,17 @@
+package baekjoon.step1;
+
+/*
+baekjoon
+step: 1
+title: 개
+url: https://www.acmicpc.net/problem/10172
+*/
+public class Dog {
+	public static void main(String[] args) {
+		System.out.println("|\\_/|\n" +
+				"|q p|   /}\n" +
+				"( 0 )\"\"\"\\\n" +
+				"|\"^\"`    |\n" +
+				"||_/=\\\\__|");
+	}
+}
