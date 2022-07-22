dce3d3b893c7327a6349bd0737584fa5604b29db
99 <hahahoho5915@gmail.com>
2022-07-22 20:48:40 +0900

    step1_새싹
    url: https://www.acmicpc.net/problem/25083

---
+++ src/baekjoon/step1/Leaf.java
@@ -0,0 +1,19 @@
+package baekjoon.step1;
+
+/*
+baekjoon
+step: 1
+title: 새싹
+url: https://www.acmicpc.net/problem/25083
+*/
+public class Leaf {
+	public static void main(String[] args) {
+		System.out.println(
+				"         ,r'\"7\n" +
+				"r`-_   ,'  ,/\n" +
+				" \\. \". L_r'\n" +
+				"   `~\\/\n" +
+				"      |\n" +
+				"      |");
+	}
+}
