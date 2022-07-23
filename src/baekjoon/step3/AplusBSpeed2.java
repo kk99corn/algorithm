1e35e2ae20634711043b6e73b253ca91ff5f05cc
99 <hahahoho5915@gmail.com>
2022-07-23 16:31:47 +0900

    step3_A+B - 7
    url: https://www.acmicpc.net/problem/11021

---
+++ src/baekjoon/step3/AplusBSpeed2.java
@@ -0,0 +1,23 @@
+package baekjoon.step3;
+
+import java.io.*;
+
+/*
+baekjoon
+step: 3
+title: A+B - 7
+url: https://www.acmicpc.net/problem/11021
+*/
+public class AplusBSpeed2 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+		int iter = Integer.parseInt(bufferedReader.readLine());
+
+		for (int i = 0; i < iter; i++) {
+			String[] arr = bufferedReader.readLine().split(" ");
+			bufferedWriter.write("Case #" + (i + 1) + ": " + (Integer.parseInt(arr[0]) + Integer.parseInt(arr[1])) + "\n");
+		}
+		bufferedWriter.close();
+	}
+}
