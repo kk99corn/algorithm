2bbeb6a0e522a124f9b9e002b4a660f163b8f74c
99 <hahahoho5915@gmail.com>
2022-11-17 20:18:26 +0900

    level1_기사단원의 무기
    url: https://school.programmers.co.kr/learn/courses/30/lessons/136798

---
+++ src/progrmmers/level1/TemplarWeapon.java
@@ -0,0 +1,45 @@
+package progrmmers.level1;
+
+import java.util.Arrays;
+
+/*
+programmers
+level: 1
+title: 기사단원의 무기
+url: https://programmers.co.kr/learn/courses/30/lessons/136798
+*/
+public class TemplarWeapon {
+
+	public int solution(int number, int limit, int power) {
+		int answer = 0;
+		for (int i = 1; i <= number; i++) {
+			int divisorCount = getDivisorCount(i);
+			answer += ((divisorCount <= limit) ? divisorCount : power);
+		}
+
+		return answer;
+	}
+
+	public int getDivisorCount(int num) {
+		int count = 0;
+		for (int i = 1; i <= Math.sqrt(num); i++) {
+			if (num % i == 0) {
+				count++;
+				if (Math.pow(i, 2) != num) {
+					count++;
+				}
+			}
+		}
+
+		return count;
+	}
+
+	public static void main(String[] args) {
+		int number = 5;
+		int limit = 3;
+		int power = 2;
+		TemplarWeapon solution = new TemplarWeapon();
+		int result = solution.solution(number, limit, power);
+		System.out.println("result = " + result);
+	}
+}
