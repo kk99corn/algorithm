af469fbd13dc79b732e81427e1ca3e06e12e2f57
99 <hahahoho5915@gmail.com>
2022-11-25 17:49:15 +0900

    level0_한 번만 등장한 문자_stream
    url: https://school.programmers.co.kr/learn/courses/30/lessons/120896

--- src/progrmmers/level0/WholeWord.java
+++ src/progrmmers/level0/WholeWord.java
@@ -1,7 +1,7 @@
 package progrmmers.level0;
 
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /*
 programmers
@@ -26,10 +26,24 @@ public class WholeWord {
 		return answer.toString();
 	}
 
+	public String solution_stream(String s) {
+		return Arrays.stream(s.split(""))
+				.collect(Collectors.groupingBy(s1 -> s1))
+				.entrySet()
+				.stream()
+				.filter(entry -> entry.getValue().size() == 1)
+				.map(Map.Entry::getKey)
+				.sorted()
+				.collect(Collectors.joining());
+	}
+
 	public static void main(String[] args) {
-		String s = "adasdasdasidoqwnuieqwbeihasdnasdnajkdandjasdnasjk";
+		String s = "aaacsdd";
 		WholeWord solution = new WholeWord();
 		String solution1 = solution.solution(s);
 		System.out.println("result = " + solution1);
+
+		String solution2 = solution.solution_stream(s);
+		System.out.println("result = " + solution2);
 	}
 }
