6f94cd1876ebf96e2a96d26b9831fb47633d998c
99 <hahahoho5915@gmail.com>
2022-12-04 17:58:43 +0900

    level0_두 수의 나눗셈
    url: https://school.programmers.co.kr/learn/courses/30/lessons/120806

---
+++ src/progrmmers/level0/TwoNumberDivide.java
@@ -0,0 +1,25 @@
+package progrmmers.level0;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+/*
+programmers
+level: 0
+title: 두 수의 나눗셈
+url: https://school.programmers.co.kr/learn/courses/30/lessons/120806
+*/
+public class TwoNumberDivide {
+
+	public int solution(int num1, int num2) {
+		return (int) (((double) num1 / num2) * 1000);
+	}
+
+	public static void main(String[] args) {
+		int n1 = 1;
+		int n2 = 16;
+		TwoNumberDivide solution = new TwoNumberDivide();
+		int solution1 = solution.solution(n1, n2);
+		System.out.println("result = " + solution1);
+	}
+}
