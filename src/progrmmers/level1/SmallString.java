756e6fee118808e97c48942618ee9f334088cd62
99 <hahahoho5915@gmail.com>
2023-02-12 22:57:28 +0900

    level1_크기가 작은 부분 문자열_stream
    url: https://school.programmers.co.kr/learn/courses/30/lessons/147355

--- src/progrmmers/level1/SmallString.java
+++ src/progrmmers/level1/SmallString.java
@@ -1,5 +1,7 @@
 package progrmmers.level1;
 
+import java.util.stream.IntStream;
+
 /*
 programmers
 level: 1
@@ -8,13 +10,21 @@ url: https://programmers.co.kr/learn/courses/30/lessons/147355
 */
 public class SmallString {
 
+	public int solutionStream(String t, String p) {
+		int pLength = p.length();
+		return (int) IntStream.range(0, t.length() - pLength + 1)
+				.filter(i -> Integer.parseInt(t.charAt(i) + "") <= Integer.parseInt(p.charAt(0) + ""))
+				.filter(i -> t.substring(i, i + (pLength)).compareTo(p) <=  0)
+				.count();
+	}
+
 	public int solution(String t, String p) {
 		int answer = 0;
 		int pLength = p.length();
 
 		for (int i = 0; i <= t.length() - pLength; i++) {
 			if (Integer.parseInt(t.charAt(i) + "") <= Integer.parseInt(p.charAt(0) + "")) {
-				if (t.substring(i, i + (pLength)).compareTo(p) > 0) answer++;
+				if (t.substring(i, i + (pLength)).compareTo(p) <= 0) answer++;
 			}
 		}
 
@@ -26,7 +36,7 @@ public class SmallString {
 		String p = "271";
 
 		SmallString dollCatch = new SmallString();
-		int solution = dollCatch.solution(t, p);
+		int solution = dollCatch.solutionStream(t, p);
 		System.out.println("solution = " + solution);
 	}
 }
