a50a5af2d0784a27409067fef8556b0077509e60
99 <hahahoho5915@gmail.com>
2023-02-14 19:46:44 +0900

    level1_옹알이 (2)_stream
    url: https://school.programmers.co.kr/learn/courses/30/lessons/147355

--- src/progrmmers/level1/Babbling.java
+++ src/progrmmers/level1/Babbling.java
@@ -1,6 +1,6 @@
 package progrmmers.level1;
 
-import java.util.stream.IntStream;
+import java.util.Arrays;
 
 /*
 programmers
@@ -10,6 +10,14 @@ title: 옹알이 (2)
 url: https://programmers.co.kr/learn/courses/30/lessons/147355
 */
 public class Babbling {
+
+	public int solution_stream(String[] babbling) {
+		return (int) Arrays.stream(babbling)
+				.filter(s -> !(s.contains("ayaaya") || s.contains("yeye") || s.contains("woowoo") || s.contains("mama")))
+				.map(s -> s.replaceAll("aya|ye|woo|ma", ""))
+				.filter(s -> s.length() == 0)
+				.count();
+	}
 
 	public int solution(String[] babbling) {
 		int answer = 0;
@@ -35,7 +43,7 @@ public class Babbling {
 		String[] babbling = {"ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"};
 
 		Babbling solution1 = new Babbling();
-		int solution = solution1.solution(babbling);
+		int solution = solution1.solution_stream(babbling);
 		System.out.println("solution = " + solution);
 	}
 }
