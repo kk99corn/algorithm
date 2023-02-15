fcd6970e8ac238e067f632296331d3f9b6000f4c
99 <hahahoho5915@gmail.com>
2023-02-15 23:17:39 +0900

    level1_약수의 개수와 덧셈_stream
    url: https://school.programmers.co.kr/learn/courses/30/lessons/77884

--- src/progrmmers/level1/DivisorCalc.java
+++ src/progrmmers/level1/DivisorCalc.java
@@ -1,5 +1,7 @@
 package progrmmers.level1;
 
+import java.util.stream.IntStream;
+
 /*
 programmers
 level: 1
@@ -7,6 +9,21 @@ title: 약수의 개수와 덧셈
 url: https://programmers.co.kr/learn/courses/30/lessons/77884
 */
 public class DivisorCalc {
+
+	public int solution_stream(int left, int right) {
+		return IntStream.range(left, right + 1)
+				.map(i -> {
+					int d = 0;
+					for (int j = 1; j <= i; j++) {
+						if (i % j == 0) {
+							d++;
+						}
+					}
+					return d % 2 == 0 ? i : -i;
+				})
+				.sum();
+	}
+
 	public int solution(int left, int right) {
 		int answer = 0;
 		for (int i = left; i <= right; i++) {
@@ -24,7 +41,7 @@ public class DivisorCalc {
 
 	public static void main(String[] args) {
 		DivisorCalc divisorCalc = new DivisorCalc();
-		int solution = divisorCalc.solution(13, 17);
+		int solution = divisorCalc.solution_stream(13, 17);
 		System.out.println("solution = " + solution);
 	}
 }
