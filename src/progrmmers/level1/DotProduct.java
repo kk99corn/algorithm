9c96e3dca0bffab3531e40908d3541d18fc6dea8
99 <hahahoho5915@gmail.com>
2023-02-16 21:41:25 +0900

    level1_내적_stream
    url: https://school.programmers.co.kr/learn/courses/30/lessons/70128

--- src/progrmmers/level1/DotProduct.java
+++ src/progrmmers/level1/DotProduct.java
@@ -1,5 +1,7 @@
 package progrmmers.level1;
 
+import java.util.stream.IntStream;
+
 /*
 programmers
 level: 1
@@ -7,6 +9,13 @@ title: 내적
 url: https://programmers.co.kr/learn/courses/30/lessons/70128
 */
 public class DotProduct {
+
+	public int solution_stream(int[] a, int[] b) {
+		return IntStream.range(0, a.length)
+				.map(i -> a[i] * b[i])
+				.sum();
+	}
+
 	public int solution(int[] a, int[] b) {
 		int answer = 0;
 		int size = a.length;
@@ -17,10 +26,10 @@ public class DotProduct {
 	}
 
 	public static void main(String[] args) {
-		int[] a = {1,2,3,4};
-		int[] b = {-3,-1,0,2};
+		int[] a = {1, 2, 3, 4};
+		int[] b = {-3, -1, 0, 2};
 		DotProduct dotProduct = new DotProduct();
-		int solution = dotProduct.solution(a, b);
+		int solution = dotProduct.solution_stream(a, b);
 		System.out.println("solution = " + solution);
 	}
 }
