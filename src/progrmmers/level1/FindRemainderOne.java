199c69443a3c1c6e16211fa95c965bdc665bf711
99 <hahahoho5915@gmail.com>
2023-02-17 18:34:41 +0900

    level1_나머지가 1이 되는 수 찾기_stream
    url: https://school.programmers.co.kr/learn/courses/30/lessons/87389

--- src/progrmmers/level1/FindRemainderOne.java
+++ src/progrmmers/level1/FindRemainderOne.java
@@ -1,5 +1,7 @@
 package progrmmers.level1;
 
+import java.util.stream.IntStream;
+
 /*
 programmers
 level: 1
@@ -7,10 +9,18 @@ title: 나머지가 1이 되는 수 찾기
 url: https://programmers.co.kr/learn/courses/30/lessons/87389
 */
 public class FindRemainderOne {
+
+	public int solution_stream(int n) {
+		return IntStream.range(2, n)
+				.filter(i -> n % i == 1)
+				.findFirst()
+				.orElse(0);
+	}
+
 	public int solution(int n) {
 		int answer = 0;
 		for (int i = 2; i < n; i++) {
-			if (n%i == 1) {
+			if (n % i == 1) {
 				answer = i;
 				break;
 			}
@@ -20,6 +30,6 @@ public class FindRemainderOne {
 
 	public static void main(String[] args) {
 		FindRemainderOne findRemainderOne = new FindRemainderOne();
-		System.out.println(findRemainderOne.solution(10));
+		System.out.println(findRemainderOne.solution_stream(10));
 	}
 }
