345082e0ba019a0ecf4675cb83a1d8849c251502
99 <hahahoho5915@gmail.com>
2023-02-18 20:31:11 +0900

    level1_숫자 문자열과 영단어_stream
    url: https://school.programmers.co.kr/learn/courses/30/lessons/81301

--- src/progrmmers/level1/NumberEnglishWords.java
+++ src/progrmmers/level1/NumberEnglishWords.java
@@ -10,6 +10,25 @@ url: https://programmers.co.kr/learn/courses/30/lessons/81301
 */
 public class NumberEnglishWords {
 
+	public int solution_stream(String s) {
+		HashMap<String, Integer> matchMap = new HashMap<>();
+		matchMap.put("zero", 0);
+		matchMap.put("one", 1);
+		matchMap.put("two", 2);
+		matchMap.put("three", 3);
+		matchMap.put("four", 4);
+		matchMap.put("five", 5);
+		matchMap.put("six", 6);
+		matchMap.put("seven", 7);
+		matchMap.put("eight", 8);
+		matchMap.put("nine", 9);
+
+		String result = matchMap.entrySet().stream()
+				.reduce(s, (acc, entry) -> acc.replace(entry.getKey(), entry.getValue().toString()), (s1, s2) -> s1);
+
+		return Integer.parseInt(result);
+	}
+
 	public int solution(String s) {
 		// 사전 데이터 세팅
 		HashMap<String, Integer> matchMap = new HashMap<>();
@@ -43,7 +62,7 @@ public class NumberEnglishWords {
 	public static void main(String[] args) {
 		NumberEnglishWords numberEnglishWords = new NumberEnglishWords();
 		String s = "one4seveneight";
-		int result = numberEnglishWords.solution(s);
+		int result = numberEnglishWords.solution_stream(s);
 		System.out.println("result = " + result);
 	}
 }
