4989c197d4618fb3008a0ae1c17c9f0186fb9d18
99 <hahahoho5915@gmail.com>
2023-02-19 20:35:56 +0900

    level1_음양 더하기_stream
    url: https://school.programmers.co.kr/learn/courses/30/lessons/76501

--- src/progrmmers/level1/SumPlusMinus.java
+++ src/progrmmers/level1/SumPlusMinus.java
@@ -1,5 +1,7 @@
 package progrmmers.level1;
 
+import java.util.stream.IntStream;
+
 /*
 programmers
 level: 1
@@ -7,8 +9,14 @@ title: 음양 더하기
 url: https://programmers.co.kr/learn/courses/30/lessons/76501
 */
 public class SumPlusMinus {
-	public int solution(int[] absolutes, boolean[] signs) {
 
+	public int solution_stream(int[] absolutes, boolean[] signs) {
+		return IntStream.range(0, absolutes.length)
+				.map(i -> signs[i] ? absolutes[i] : -absolutes[i])
+				.sum();
+	}
+
+	public int solution(int[] absolutes, boolean[] signs) {
 		int answer = 0;
 		for (int i = 0; i < absolutes.length; i++) {
 			answer += absolutes[i] * (signs[i] ? 1 : -1);
@@ -23,5 +31,8 @@ public class SumPlusMinus {
 		SumPlusMinus sumPlusMinus = new SumPlusMinus();
 		int solution = sumPlusMinus.solution(absolutes, signs);
 		System.out.println("solution = " + solution);
+
+		int solution2 = sumPlusMinus.solution_stream(absolutes, signs);
+		System.out.println("solution = " + solution2);
 	}
 }
