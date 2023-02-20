5ae9b1ed29eadfc12c81408963c21576162e6367
99 <hahahoho5915@gmail.com>
2023-02-20 19:59:25 +0900

    level1_기사단원의 무기_stream
    url: https://school.programmers.co.kr/learn/courses/30/lessons/136798

--- src/progrmmers/level1/TemplarWeapon.java
+++ src/progrmmers/level1/TemplarWeapon.java
@@ -1,6 +1,6 @@
 package progrmmers.level1;
 
-import java.util.Arrays;
+import java.util.stream.IntStream;
 
 /*
 programmers
@@ -9,6 +9,21 @@ title: 기사단원의 무기
 url: https://programmers.co.kr/learn/courses/30/lessons/136798
 */
 public class TemplarWeapon {
+
+	public int solution_stream(int number, int limit, int power) {
+		return IntStream.rangeClosed(1, number)
+				.map(this::getDivisorCount_stream)
+				.map(divisorCount -> (divisorCount <= limit) ? divisorCount : power)
+				.sum();
+	}
+
+	public int getDivisorCount_stream(int num) {
+		return (int) IntStream.rangeClosed(1, (int) Math.sqrt(num))
+				.filter(i -> num % i == 0)
+				.map(i -> (num / i == i) ? 1 : 2)
+				.sum();
+	}
+
 
 	public int solution(int number, int limit, int power) {
 		int answer = 0;
@@ -41,5 +56,8 @@ public class TemplarWeapon {
 		TemplarWeapon solution = new TemplarWeapon();
 		int result = solution.solution(number, limit, power);
 		System.out.println("result = " + result);
+
+		int result2 = solution.solution_stream(number, limit, power);
+		System.out.println("result2 = " + result2);
 	}
 }
