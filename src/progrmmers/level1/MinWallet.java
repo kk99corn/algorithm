5348b22194feadb57a2deef865e58094e035bad0
99 <hahahoho5915@gmail.com>
2023-02-21 23:10:36 +0900

    level1_최소직사각형_stream
    url: https://school.programmers.co.kr/learn/courses/30/lessons/86491

--- src/progrmmers/level1/MinWallet.java
+++ src/progrmmers/level1/MinWallet.java
@@ -1,5 +1,7 @@
 package progrmmers.level1;
 
+import java.util.Arrays;
+
 /*
 programmers
 level: 1
@@ -7,6 +9,14 @@ title: 최소직사각형
 url: https://programmers.co.kr/learn/courses/30/lessons/86491
 */
 public class MinWallet {
+
+	public int solution_stream(int[][] sizes) {
+		int[] maxSizes = Arrays.stream(sizes)
+				.map(size -> new int[]{Math.max(size[0], size[1]), Math.min(size[0], size[1])})
+				.reduce(new int[]{0, 0}, (acc, size) -> new int[]{Math.max(acc[0], size[0]), Math.max(acc[1], size[1])});
+		return maxSizes[0] * maxSizes[1];
+	}
+
 	public int solution(int[][] sizes) {
 		int width = 0;
 		int height = 0;
@@ -24,5 +34,8 @@ public class MinWallet {
 		MinWallet minWallet = new MinWallet();
 		int result = minWallet.solution(sizes);
 		System.out.println("result = " + result);
+
+		int result2 = minWallet.solution_stream(sizes);
+		System.out.println("result2 = " + result2);
 	}
 }
