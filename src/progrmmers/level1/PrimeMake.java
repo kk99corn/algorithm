4be521dbedf7ad5630c718d09fb61658cfb642fa
99 <hahahoho5915@gmail.com>
2023-02-27 18:25:08 +0900

    level1_소수 만들기_stream
    url: https://school.programmers.co.kr/learn/courses/30/lessons/12977

--- src/progrmmers/level1/PrimeMake.java
+++ src/progrmmers/level1/PrimeMake.java
@@ -1,6 +1,7 @@
 package progrmmers.level1;
 
 import java.util.ArrayList;
+import java.util.stream.IntStream;
 
 /*
 programmers
@@ -52,10 +53,32 @@ public class PrimeMake {
 		return isPrime;
 	}
 
+	public int solution_stream(int[] nums) {
+		return (int) IntStream.range(0, nums.length - 2)
+				.flatMap(i -> IntStream.range(i + 1, nums.length - 1)
+						.flatMap(j -> IntStream.range(j + 1, nums.length)
+								.map(x -> nums[i] + nums[j] + nums[x])
+						)
+				)
+				.filter(this::isPrime_stream)
+				.count();
+	}
+
+	boolean isPrime_stream(int num) {
+		if (num < 2) {
+			return false;
+		}
+		return IntStream.rangeClosed(2, (int) Math.sqrt(num))
+				.noneMatch(i -> num % i == 0);
+	}
+
 	public static void main(String[] args) {
 		PrimeMake primeMake = new PrimeMake();
 		int[] nums = {1, 2, 7, 6, 4};
 		int solution = primeMake.solution(nums);
 		System.out.println("solution = " + solution);
+
+		int solution2 = primeMake.solution_stream(nums);
+		System.out.println("solution2 = " + solution2);
 	}
 }
