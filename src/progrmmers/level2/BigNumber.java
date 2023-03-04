a6572359fbeec33000a4dfabcc5ff4e905e9f86e
99 <hahahoho5915@gmail.com>
2023-03-04 19:39:47 +0900

    level2_큰 수 만들기_stream
    url: https://school.programmers.co.kr/learn/courses/30/lessons/42883

--- src/progrmmers/level2/BigNumber.java
+++ src/progrmmers/level2/BigNumber.java
@@ -1,5 +1,8 @@
 package progrmmers.level2;
 
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
 /*
 programmers
 level: 2
@@ -35,11 +38,43 @@ public class BigNumber {
 		return answer.toString();
 	}
 
+	public String solution_stream(String number, int k) {
+		StringBuilder answer = new StringBuilder(number);
+
+		for (int i = 0; i < k; i++) {
+			AtomicInteger current = new AtomicInteger();
+			boolean deleteCheck = IntStream.range(current.get(), answer.length() - 1)
+					.filter(j -> {
+						if (j > 0) current.set(j - 1);
+						return answer.charAt(j) < answer.charAt(j + 1);
+					})
+					.findFirst()
+					.isPresent(); // OptionalInt 대신 isPresent()를 사용합니다.
+
+			if (deleteCheck) {
+				int j = IntStream.range(current.get(), answer.length() - 1)
+						.filter(index -> answer.charAt(index) < answer.charAt(index + 1))
+						.findFirst()
+						.getAsInt(); // getAsInt()를 사용해 정수형으로 변환합니다.
+				answer.deleteCharAt(j);
+			} else {
+				IntStream.range(0, k - i)
+						.forEach(index -> answer.deleteCharAt(answer.length() - 1));
+				break;
+			}
+		}
+
+		return answer.toString();
+	}
+
 	public static void main(String[] args) {
 		String number = "98721314";
 		int k = 3;
 		BigNumber bigNumber = new BigNumber();
 		String solution = bigNumber.solution(number, 4);
 		System.out.println("solution = " + solution);
+
+		String solution2 = bigNumber.solution_stream(number, 4);
+		System.out.println("solution2 = " + solution2);
 	}
 }
