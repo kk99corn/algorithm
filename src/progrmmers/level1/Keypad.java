918b89e33d3052a2f86b1d75c81ee1fae18afdf2
99 <hahahoho5915@gmail.com>
2023-03-14 18:26:56 +0900

    level1_키패드 누르기
    url: https://school.programmers.co.kr/learn/courses/30/lessons/67256

---
+++ src/progrmmers/level1/Keypad.java
@@ -0,0 +1,77 @@
+package progrmmers.level1;
+
+import java.util.*;
+
+/*
+programmers
+level: 1
+title: 키패드 누르기
+url: https://school.programmers.co.kr/learn/courses/30/lessons/67256
+*/
+public class Keypad {
+
+	public String solution(int[] numbers, String hand) {
+		Map<Integer, List<Integer>> map = new HashMap<>();
+		map.put(1, new ArrayList<>(List.of(0, 0)));
+		map.put(2, new ArrayList<>(List.of(0, 1)));
+		map.put(3, new ArrayList<>(List.of(0, 2)));
+		map.put(4, new ArrayList<>(List.of(1, 0)));
+		map.put(5, new ArrayList<>(List.of(1, 1)));
+		map.put(6, new ArrayList<>(List.of(1, 2)));
+		map.put(7, new ArrayList<>(List.of(2, 0)));
+		map.put(8, new ArrayList<>(List.of(2, 1)));
+		map.put(9, new ArrayList<>(List.of(2, 2)));
+		map.put(0, new ArrayList<>(List.of(3, 1)));
+
+		String answer = "";
+		String selectedHand = "";
+
+		int[] currentL = {3, 0};
+		int[] currentR = {3, 2};
+
+		int distanceLtoX;
+		int distanceRtoX;
+
+		for (int n : numbers) {
+			if (n == 1 || n == 4 || n == 7) {
+				selectedHand = "L";
+			} else if (n == 3 || n == 6 || n == 9) {
+				selectedHand = "R";
+			} else {
+				distanceLtoX = Math.abs(currentL[0] - map.get(n).get(0)) + Math.abs(currentL[1] - map.get(n).get(1));
+				distanceRtoX = Math.abs(currentR[0] - map.get(n).get(0)) + Math.abs(currentR[1] - map.get(n).get(1));
+
+				if (distanceLtoX == distanceRtoX) {
+					if (hand.equals("left")) {
+						selectedHand = "L";
+					} else {
+						selectedHand = "R";
+					}
+				} else if (distanceLtoX < distanceRtoX) {
+					selectedHand = "L";
+				} else {
+					selectedHand = "R";
+				}
+			}
+
+			if (selectedHand.equals("L")) {
+				currentL[0] = map.get(n).get(0);
+				currentL[1] = map.get(n).get(1);
+			} else {
+				currentR[0] = map.get(n).get(0);
+				currentR[1] = map.get(n).get(1);
+			}
+			answer += selectedHand;
+		}
+
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		int[] numbers = {1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5};
+		String hand = "right";
+		Keypad solution = new Keypad();
+		String solution1 = solution.solution(numbers, hand);
+		System.out.println("result = " + (solution1));
+	}
+}
