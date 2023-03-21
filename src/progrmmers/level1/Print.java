2a5798e9a489aa0f03fc41ad12c1caef7559f41c
99 <hahahoho5915@gmail.com>
2023-03-21 18:36:06 +0900

    level1_덧칠하기
    url: https://school.programmers.co.kr/learn/courses/30/lessons/161989

---
+++ src/progrmmers/level1/Print.java
@@ -0,0 +1,32 @@
+package progrmmers.level1;
+
+/*
+programmers
+level: 1
+title: 덧칠하기
+url: https://school.programmers.co.kr/learn/courses/30/lessons/161989
+*/
+public class Print {
+
+	public int solution(int n, int m, int[] section) {
+		int answer = 1;
+		int p = section[0] + m - 1;
+		for (int s : section) {
+			if (s > p) {
+				p = s + m - 1;
+				answer++;
+			}
+		}
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		int n = 8;
+		int m = 4;
+		int[] section = {2, 3, 6};
+
+		Print solution = new Print();
+		int solution1 = solution.solution(n, m, section);
+		System.out.println("result = " + (solution1));
+	}
+}
