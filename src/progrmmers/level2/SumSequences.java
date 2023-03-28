665d04ff4a7b504e5d0ab5deb8feceb7471b8c15
99 <hahahoho5915@gmail.com>
2023-03-28 18:24:38 +0900

    level2_연속 부분 수열 합의 개수_stream
    url: https://school.programmers.co.kr/learn/courses/30/lessons/131701

--- src/progrmmers/level2/SumSequences.java
+++ src/progrmmers/level2/SumSequences.java
@@ -2,6 +2,8 @@ package progrmmers.level2;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /*
 programmers
@@ -10,6 +12,21 @@ title: 연속 부분 수열 합의 개수
 url: https://programmers.co.kr/learn/courses/30/lessons/131701
 */
 public class SumSequences {
+
+	public int solutionStream(int[] elements) {
+		Set<Integer> numberSet = new HashSet<>();
+
+		IntStream.range(0, elements.length)
+				.forEach(i -> {
+					int sum = 0;
+					for (int j = i; j < i + elements.length; j++) {
+						sum += elements[j % elements.length];
+						numberSet.add(sum);
+					}
+				});
+
+		return numberSet.size();
+	}
 
 	public int solution(int[] elements) {
 		Set<Integer> numberSet = new HashSet<>();
@@ -34,5 +51,8 @@ public class SumSequences {
 		SumSequences bigNumber = new SumSequences();
 		int solution = bigNumber.solution(elements);
 		System.out.println("solution = " + solution);
+
+		int solution2 = bigNumber.solutionStream(elements);
+		System.out.println("solution = " + solution2);
 	}
 }
