39feab6e4e6b915423b1db3258ff6501fb963ec7
99 <hahahoho5915@gmail.com>
2023-03-30 18:22:09 +0900

    level2_귤 고르기_stream
    url: https://school.programmers.co.kr/learn/courses/30/lessons/138476

--- src/progrmmers/level2/Orange.java
+++ src/progrmmers/level2/Orange.java
@@ -1,6 +1,8 @@
 package progrmmers.level2;
 
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /*
 programmers
@@ -9,6 +11,24 @@ title: 귤 고르기
 url: https://programmers.co.kr/learn/courses/30/lessons/138476
 */
 public class Orange {
+
+	public int solutionStream(int k, int[] tangerine) {
+		Map<Integer, Integer> map = Arrays.stream(tangerine)
+				.boxed()
+				.collect(Collectors.toMap(Function.identity(), t -> 1, Integer::sum));
+
+		List<Integer> keyList = map.keySet().stream()
+				.sorted((o1, o2) -> map.get(o2).compareTo(map.get(o1)))
+				.collect(Collectors.toList());
+
+		int[] sum = new int[1];
+		return (int) keyList.stream()
+				.takeWhile(key -> {
+					sum[0] += map.get(key);
+					return k >= sum[0];
+				})
+				.count();
+	}
 
 	public int solution(int k, int[] tangerine) {
 		int answer = 0;
@@ -40,5 +60,8 @@ public class Orange {
 		Orange biggestNumber = new Orange();
 		int solution = biggestNumber.solution(k, tangerine);
 		System.out.println("solution = " + solution);
+
+		int solution2 = biggestNumber.solutionStream(k, tangerine);
+		System.out.println("solution = " + solution2);
 	}
 }
