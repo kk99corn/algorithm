214771459f52e620413397ab478fc0855eddb8e5
99 <hahahoho5915@gmail.com>
2023-04-05 20:25:52 +0900

    level1_햄버거 만들기_stream
    url: https://school.programmers.co.kr/learn/courses/30/lessons/133502

--- src/progrmmers/level1/Hamburger.java
+++ src/progrmmers/level1/Hamburger.java
@@ -1,6 +1,7 @@
 package progrmmers.level1;
 
 import java.util.Stack;
+import java.util.stream.IntStream;
 
 /*
 programmers
@@ -9,6 +10,28 @@ title: 햄버거 만들기
 url: https://school.programmers.co.kr/learn/courses/30/lessons/133502
 */
 public class Hamburger {
+
+	public int solutionStream(int[] ingredient) {
+		Stack<Integer> stack = new Stack<>();
+		return (int) IntStream.of(ingredient)
+				.filter(i -> {
+					stack.push(i);
+					int size = stack.size();
+					if (size >= 4) {
+						if (stack.get(size - 4) == 1
+								&& stack.get(size - 3) == 2
+								&& stack.get(size - 2) == 3
+								&& stack.get(size - 1) == 1) {
+							for (int j = 0; j < 4; j++) {
+								stack.pop();
+							}
+							return true;
+						}
+					}
+					return false;
+				})
+				.count();
+	}
 
 	public int solution(int[] ingredient) {
 		int answer = 0;
@@ -40,5 +63,8 @@ public class Hamburger {
 		Hamburger solution = new Hamburger();
 		int solution1 = solution.solution(ingredient);
 		System.out.println("result = " + (solution1));
+
+		int solution2 = solution.solutionStream(ingredient);
+		System.out.println("result = " + (solution2));
 	}
 }
