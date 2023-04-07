a4d8beb7b707c489717a6798b6579d72722f7b5f
99 <hahahoho5915@gmail.com>
2023-04-07 19:42:33 +0900

    level2_방문 길이_stream
    url: https://school.programmers.co.kr/learn/courses/30/lessons/49994

--- src/progrmmers/level2/VisitLength.java
+++ src/progrmmers/level2/VisitLength.java
@@ -10,6 +10,42 @@ title: 방문 길이
 url: https://programmers.co.kr/learn/courses/30/lessons/49994
 */
 public class VisitLength {
+	public int solutionStream(String dirs) {
+		Set<String> set = new HashSet<>();
+
+		int max = 5;
+		int min = -5;
+
+		int[] pos = {0, 0};
+
+		dirs.chars().forEach(c -> {
+			String current = pos[0] + "" + pos[1];
+			switch (c) {
+				case 'U': // 위
+					if (pos[1] < max) pos[1]++;
+					break;
+				case 'D': // 아래
+					if (pos[1] > min) pos[1]--;
+					break;
+				case 'R': // 오른쪽
+					if (pos[0] < max) pos[0]++;
+					break;
+				case 'L': // 왼쪽
+					if (pos[0] > min) pos[0]--;
+					break;
+			}
+
+			// move position
+			String next = pos[0] + "" + pos[1];
+			if (!current.equals(next)) {
+				set.add(current + next);
+				set.add(next + current);
+			}
+		});
+
+		return set.size() / 2;
+	}
+
 	public int solution(String dirs) {
 		Set<String> set = new HashSet<>();
 
@@ -24,16 +60,16 @@ public class VisitLength {
 			// 현재 위치
 			String current = Integer.toString(x) + y;
 			switch (dirs.charAt(i)) {
-				case 'U':	// 위
+				case 'U':    // 위
 					if (y < max) y++;
 					break;
-				case 'D':	// 아래
+				case 'D':    // 아래
 					if (y > min) y--;
 					break;
-				case 'R':	// 오른쪽
+				case 'R':    // 오른쪽
 					if (x < max) x++;
 					break;
-				case 'L':	// 왼쪽
+				case 'L':    // 왼쪽
 					if (x > min) x--;
 					break;
 			}
@@ -55,5 +91,8 @@ public class VisitLength {
 		VisitLength visitLength = new VisitLength();
 		int solution = visitLength.solution(dirs);
 		System.out.println("solution = " + solution);
+
+		int solution2 = visitLength.solutionStream(dirs);
+		System.out.println("solution = " + solution2);
 	}
 }
