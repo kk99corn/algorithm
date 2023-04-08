2029e9497b5dc8c22b28f9ec1760e89ab6cc9d27
99 <hahahoho5915@gmail.com>
2023-04-08 22:06:49 +0900

    level2_카펫_stream
    url: https://school.programmers.co.kr/learn/courses/30/lessons/42842

--- src/progrmmers/level2/Carpet.java
+++ src/progrmmers/level2/Carpet.java
@@ -1,6 +1,7 @@
 package progrmmers.level2;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 /*
 programmers
@@ -9,6 +10,17 @@ title: 카펫
 url: https://programmers.co.kr/learn/courses/30/lessons/42842
 */
 public class Carpet {
+
+	public int[] solutionStream(int brown, int yellow) {
+		int sum = brown + yellow;
+		return IntStream.rangeClosed(3, sum)
+				.filter(i -> sum % i == 0 && sum / i >= 3)
+				.mapToObj(i -> new int[] {Math.max(i, sum / i), Math.min(i, sum / i)})
+				.filter(dimensions -> yellow == (dimensions[0] - 2) * (dimensions[1] - 2))
+				.findFirst()
+				.orElse(new int[2]);
+	}
+
 	public int[] solution(int brown, int yellow) {
 		int[] answer = new int[2];
 		int sum = brown + yellow;
@@ -34,5 +46,8 @@ public class Carpet {
 		Carpet carpet = new Carpet();
 		int[] solution = carpet.solution(24, 24);
 		System.out.println("solution = " + Arrays.toString(solution));
+
+		int[] solution2 = carpet.solutionStream(24, 24);
+		System.out.println("solution = " + Arrays.toString(solution2));
 	}
 }
