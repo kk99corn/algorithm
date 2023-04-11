fa138e4d334645382ccb2e54eb3ae5e9689a7709
99 <hahahoho5915@gmail.com>
2023-04-11 18:37:12 +0900

    level2_삼각 달팽이_stream
    url: https://school.programmers.co.kr/learn/courses/30/lessons/68645

--- src/progrmmers/level2/Snail.java
+++ src/progrmmers/level2/Snail.java
@@ -1,6 +1,8 @@
 package progrmmers.level2;
 
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
 
 /*
 programmers
@@ -9,6 +11,30 @@ title: 삼각 달팽이
 url: https://programmers.co.kr/learn/courses/30/lessons/68645
 */
 public class Snail {
+
+	public int[] solutionStream(int n) {
+		int[][] matrix = new int[n][n];
+		AtomicInteger x = new AtomicInteger(-1);
+		AtomicInteger y = new AtomicInteger();
+		AtomicInteger num = new AtomicInteger(1);
+		IntStream.range(0, n).forEach(i ->
+				IntStream.range(i, n).forEach(j -> {
+					switch (i % 3) {
+						case 0 -> x.getAndIncrement();
+						case 1 -> y.getAndIncrement();
+						case 2 -> {
+							x.getAndDecrement();
+							y.getAndDecrement();
+						}
+					}
+					matrix[x.get()][y.get()] = num.getAndIncrement();
+				})
+		);
+		return Arrays.stream(matrix)
+				.flatMapToInt(Arrays::stream)
+				.filter(number -> number != 0)
+				.toArray();
+	}
 
 	public int[] solution(int n) {
 		int[] answer = new int[(n * (n + 1)) / 2];
@@ -45,7 +71,10 @@ public class Snail {
 
 	public static void main(String[] args) {
 		Snail solutions = new Snail();
-		int[] solution = solutions.solution(4);
+		int[] solution = solutions.solution(5);
 		System.out.println("solution = " + Arrays.toString(solution));
+
+		int[] solution2 = solutions.solutionStream(5);
+		System.out.println("solution = " + Arrays.toString(solution2));
 	}
 }
