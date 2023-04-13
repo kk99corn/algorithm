839dba141ffb9f36a006a248f50ce55848ec7f21
99 <hahahoho5915@gmail.com>
2023-04-13 19:37:53 +0900

    level2_점프와 순간 이동_stream
    url: https://school.programmers.co.kr/learn/courses/30/lessons/12980

--- src/progrmmers/level2/JumpSuits.java
+++ src/progrmmers/level2/JumpSuits.java
@@ -1,5 +1,7 @@
 package progrmmers.level2;
 
+import java.util.stream.IntStream;
+
 /*
 programmers
 level: 2
@@ -7,6 +9,13 @@ title: 점프와 순간 이동
 url: https://programmers.co.kr/learn/courses/30/lessons/12980
 */
 public class JumpSuits {
+
+	public int solutionStream(int n) {
+		return IntStream.iterate(n, i -> i != 0, i -> i % 2 == 0 ? i / 2 : i - 1)
+				.map(i -> i % 2)
+				.sum();
+	}
+
 	public int solution(int n) {
 		int ans = 0;
 		while (n != 0) {
@@ -24,5 +33,8 @@ public class JumpSuits {
 		JumpSuits jumpSuits = new JumpSuits();
 		int solution = jumpSuits.solution(5000);
 		System.out.println(solution);
+
+		int solution2 = jumpSuits.solutionStream(5000);
+		System.out.println(solution2);
 	}
 }
