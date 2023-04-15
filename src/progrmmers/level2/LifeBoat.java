863a8f8cd15d9e25ecaa4da8495fccc9540af625
99 <hahahoho5915@gmail.com>
2023-04-15 17:36:01 +0900

    level2_구명보트_stream
    url: https://school.programmers.co.kr/learn/courses/30/lessons/42885

--- src/progrmmers/level2/LifeBoat.java
+++ src/progrmmers/level2/LifeBoat.java
@@ -3,6 +3,9 @@ package progrmmers.level2;
 import java.util.Arrays;
 import java.util.Deque;
 import java.util.LinkedList;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /*
 programmers
@@ -11,6 +14,23 @@ title: 구명보트
 url: https://programmers.co.kr/learn/courses/30/lessons/42885
 */
 public class LifeBoat {
+
+	public int solutionStream(int[] people, int limit) {
+		int[] sortedPeople = Arrays.stream(people).sorted().toArray();
+
+		Deque<Integer> deque = Arrays.stream(sortedPeople).boxed().collect(Collectors.toCollection(LinkedList::new));
+
+		return Stream.iterate(0, Objects::nonNull, i -> deque.isEmpty() ? null : i + 1)
+				.map(i -> {
+					int sum = deque.pollLast();
+					if (sum <= limit && !deque.isEmpty() && (limit - sum) >= deque.peekFirst()) {
+						deque.pollFirst();
+					}
+					return 1;
+				})
+				.reduce(0, Integer::sum);
+	}
+
 	public int solution(int[] people, int limit) {
 		// people 정렬
 		Arrays.sort(people);
@@ -41,5 +61,8 @@ public class LifeBoat {
 		LifeBoat lifeBoat = new LifeBoat();
 		int solution = lifeBoat.solution(people, limit);
 		System.out.println("solution = " + solution);
+
+		int solution2 = lifeBoat.solutionStream(people, limit);
+		System.out.println("solution = " + solution2);
 	}
 }
