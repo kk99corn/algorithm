9309ae1b3584426371a957a61833a066ad484cfb
99 <hahahoho5915@gmail.com>
2023-04-30 16:15:43 +0900

    level0_배열 만들기 6
    url: https://school.programmers.co.kr/learn/courses/30/lessons/181859

---
+++ src/progrmmers/level0/MakeArray6.java
@@ -0,0 +1,41 @@
+package progrmmers.level0;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+/*
+programmers
+level: 0
+title: 배열 만들기 6
+url: https://school.programmers.co.kr/learn/courses/30/lessons/181859
+*/
+public class MakeArray6 {
+
+	public int[] solution(int[] arr) {
+		Stack<Integer> stack = new Stack<>();
+		int[] answer = {-1};
+
+		for (int n : arr) {
+			if (stack.isEmpty()) {
+				stack.push(n);
+			} else {
+				int top = stack.peek();
+				if (top == n) {
+					stack.pop();
+				} else {
+					stack.push(n);
+				}
+			}
+		}
+
+		if (!stack.isEmpty()) answer = stack.stream().mapToInt(Integer::intValue).toArray();
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = {0, 1, 1, 1, 0};
+		MakeArray6 solution = new MakeArray6();
+		int[] solution1 = solution.solution(arr);
+		System.out.println("result = " + Arrays.toString(solution1));
+	}
+}
