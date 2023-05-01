1effedc43c1e9f554e6ab5746a8d16e50f67e5fd
99 <hahahoho5915@gmail.com>
2023-05-01 17:31:53 +0900

    level0_배열 만들기 6_stream
    url: https://school.programmers.co.kr/learn/courses/30/lessons/181859

--- src/progrmmers/level0/MakeArray6.java
+++ src/progrmmers/level0/MakeArray6.java
@@ -2,6 +2,7 @@ package progrmmers.level0;
 
 import java.util.Arrays;
 import java.util.Stack;
+import java.util.stream.IntStream;
 
 /*
 programmers
@@ -10,6 +11,30 @@ title: 배열 만들기 6
 url: https://school.programmers.co.kr/learn/courses/30/lessons/181859
 */
 public class MakeArray6 {
+
+	public int[] solutionStream(int[] arr) {
+		Stack<Integer> stack = new Stack<>();
+
+		int[] answer = IntStream.of(arr)
+				.filter(n -> {
+					if (stack.isEmpty()) {
+						stack.push(n);
+						return false;
+					} else {
+						int top = stack.peek();
+						if (top == n) {
+							stack.pop();
+							return true;
+						} else {
+							stack.push(n);
+							return false;
+						}
+					}
+				})
+				.toArray();
+
+		return stack.isEmpty() ? answer : stack.stream().mapToInt(Integer::intValue).toArray();
+	}
 
 	public int[] solution(int[] arr) {
 		Stack<Integer> stack = new Stack<>();
@@ -37,5 +62,8 @@ public class MakeArray6 {
 		MakeArray6 solution = new MakeArray6();
 		int[] solution1 = solution.solution(arr);
 		System.out.println("result = " + Arrays.toString(solution1));
+
+		int[] solution2 = solution.solutionStream(arr);
+		System.out.println("result = " + Arrays.toString(solution2));
 	}
 }
