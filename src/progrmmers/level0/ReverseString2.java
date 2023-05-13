c7e3859ea559560b736766570ea3de65aeb716eb
99 <hahahoho5915@gmail.com>
2023-05-14 03:05:48 +0900

    level0_문자열 여러 번 뒤집기_Stream
    url: https://school.programmers.co.kr/learn/courses/30/lessons/181913

--- src/progrmmers/level0/ReverseString2.java
+++ src/progrmmers/level0/ReverseString2.java
@@ -1,5 +1,7 @@
 package progrmmers.level0;
 
+import java.util.Arrays;
+
 /*
 programmers
 level: 0
@@ -7,6 +9,13 @@ title: 문자열 여러 번 뒤집기
 url: https://school.programmers.co.kr/learn/courses/30/lessons/181913
 */
 public class ReverseString2 {
+
+	public String solutionStream(String my_string, int[][] queries) {
+		StringBuilder sb = new StringBuilder(my_string);
+		return Arrays.stream(queries)
+				.reduce(sb, (s, query) -> s.replace(query[0], query[1] + 1, new StringBuilder(s.substring(query[0], query[1] + 1)).reverse().toString()), (s1, s2) -> s1)
+				.toString();
+	}
 
 	public String solution(String my_string, int[][] queries) {
 		StringBuilder sb = new StringBuilder(my_string);
@@ -23,5 +32,8 @@ public class ReverseString2 {
 		int[][] queries = {{2, 3}, {0, 7}, {5, 9}, {6, 10}};
 		String solution1 = solution.solution(my_string, queries);
 		System.out.println("result = " + (solution1));
+
+		String solution2 = solution.solutionStream(my_string, queries);
+		System.out.println("result = " + (solution2));
 	}
 }
