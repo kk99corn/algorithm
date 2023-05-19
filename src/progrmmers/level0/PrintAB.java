b504e94ed360df938991a92937d90ed19c59c8f6
99 <hahahoho5915@gmail.com>
2023-05-19 18:35:00 +0900

    level0_a와 b 출력하기
    url: https://school.programmers.co.kr/learn/courses/30/lessons/181951

---
+++ src/progrmmers/level0/PrintAB.java
@@ -0,0 +1,21 @@
+package progrmmers.level0;
+
+import java.util.Scanner;
+
+/*
+programmers
+level: 0
+title: a와 b 출력하기
+url: https://school.programmers.co.kr/learn/courses/30/lessons/181951
+*/
+public class PrintAB {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+	}
+}
