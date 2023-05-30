ae30c6fe9b7ce564dd6a8ae4e6acad45a753c221
99 <hahahoho5915@gmail.com>
2023-05-30 18:35:50 +0900

    level0_문자 개수 세기_stream
    url: https://school.programmers.co.kr/learn/courses/30/lessons/181902

--- src/progrmmers/level0/CountString.java
+++ src/progrmmers/level0/CountString.java
@@ -10,6 +10,29 @@ url: https://school.programmers.co.kr/learn/courses/30/lessons/181902
 */
 public class CountString {
 
+	public int[] solutionStream(String my_string) {
+		return my_string.chars()
+				.map(this::getIndex)
+				.filter(idx -> idx >= 0 && idx < 52)
+				.collect(
+						() -> new int[52],
+						(arr, idx) -> arr[idx] += 1,
+						(arr1, arr2) -> {
+							for (int i = 0; i < arr1.length; i++) {
+								arr1[i] += arr2[i];
+							}
+						}
+				);
+	}
+
+	private int getIndex(int charIdx) {
+		if (charIdx >= 65 && charIdx <= 90) {
+			return charIdx - 65;
+		} else {
+			return charIdx - 71;
+		}
+	}
+
 	public int[] solution(String my_string) {
 		int[] answer = new int[52];
 		for (int i = 0; i <my_string.length(); i++) {
@@ -27,7 +50,9 @@ public class CountString {
 		CountString solution = new CountString();
 		String my_string = "Programmers";
 		int[] solution1 = solution.solution(my_string);
-
 		System.out.println("result = " + Arrays.toString(solution1));
+
+		int[] solution2 = solution.solutionStream(my_string);
+		System.out.println("result = " + Arrays.toString(solution2));
 	}
 }
