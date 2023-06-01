c8d2ef1947f7e63f68d3aa4f5aed72f617c6e95f
99 <hahahoho5915@gmail.com>
2023-06-01 19:17:20 +0900

    level0_커피 심부름_stream
    url: https://school.programmers.co.kr/learn/courses/30/lessons/181837

--- src/progrmmers/level0/Coffee.java
+++ src/progrmmers/level0/Coffee.java
@@ -1,5 +1,7 @@
 package progrmmers.level0;
 
+import java.util.Arrays;
+
 /*
 programmers
 level: 0
@@ -7,6 +9,12 @@ title: 커피 심부름
 url: https://school.programmers.co.kr/learn/courses/30/lessons/181837
 */
 public class Coffee {
+
+	public int solutionStream(String[] order) {
+		return Arrays.stream(order)
+				.mapToInt(s -> s.contains("cafelatte") ? 5000 : 4500)
+				.sum();
+	}
 
 	public int solution(String[] order) {
 		int answer = 0;
@@ -23,8 +31,11 @@ public class Coffee {
 	public static void main(String[] args) {
 		Coffee solution = new Coffee();
 
-		String[] order= {"cafelatte", "americanoice", "hotcafelatte", "anything"};
+		String[] order = {"cafelatte", "americanoice", "hotcafelatte", "anything"};
 		int solution1 = solution.solution(order);
 		System.out.println("result = " + (solution1));
+
+		int solution2 = solution.solutionStream(order);
+		System.out.println("result = " + (solution2));
 	}
 }
