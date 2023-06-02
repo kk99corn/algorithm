7d4200641beda80fa453d789d48a73b1474a6cd3
99 <hahahoho5915@gmail.com>
2023-06-02 16:39:29 +0900

    level0_2의 영역_stream
    url: https://school.programmers.co.kr/learn/courses/30/lessons/181894

--- src/progrmmers/level0/Area2.java
+++ src/progrmmers/level0/Area2.java
@@ -1,6 +1,7 @@
 package progrmmers.level0;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 /*
 programmers
@@ -9,6 +10,24 @@ title: 2의 영역
 url: https://school.programmers.co.kr/learn/courses/30/lessons/181894
 */
 public class Area2 {
+
+	public int[] solutionStream(int[] arr) {
+		int s = IntStream.range(0, arr.length)
+				.filter(i -> arr[i] == 2)
+				.findFirst()
+				.orElse(-1);
+
+		int e = IntStream.range(s + 1, arr.length)
+				.filter(i -> arr[i] == 2)
+				.findFirst()
+				.orElse(-1);
+
+		if (s != -1 && e != -1) {
+			return Arrays.copyOfRange(arr, s, e + 1);
+		} else {
+			return (s != -1) ? new int[]{2} : new int[]{-1};
+		}
+	}
 
 	public int[] solution(int[] arr) {
 		Integer s = null, e = null;
@@ -41,5 +60,8 @@ public class Area2 {
 		int[] arr = {1, 2, 1, 4, 5, 2, 9};
 		int[] solution1 = solution.solution(arr);
 		System.out.println("result = " + Arrays.toString(solution1));
+
+		int[] solution2 = solution.solutionStream(arr);
+		System.out.println("result = " + Arrays.toString(solution2));
 	}
 }
