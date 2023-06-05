64d05789fc0739c0d651a86feb995593088bb53b
99 <hahahoho5915@gmail.com>
2023-06-05 18:54:08 +0900

    level0_문자열이 몇 번 등장하는지 세기_stream
    url: https://school.programmers.co.kr/learn/courses/30/lessons/181871

--- src/progrmmers/level0/StringCounting.java
+++ src/progrmmers/level0/StringCounting.java
@@ -1,5 +1,8 @@
 package progrmmers.level0;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 /*
 programmers
 level: 0
@@ -7,6 +10,20 @@ title: 문자열이 몇 번 등장하는지 세기
 url: https://school.programmers.co.kr/learn/courses/30/lessons/181871
 */
 public class StringCounting {
+
+	public int solutionStream(String myString, String pat) {
+		int answer = 0;
+		for (int i = 0; i <= myString.length() - pat.length(); i++) {
+			String temp = IntStream.range(i, i + pat.length())
+					.mapToObj(j -> String.valueOf(myString.charAt(j)))
+					.collect(Collectors.joining());
+
+			if (pat.equals(temp)) {
+				answer++;
+			}
+		}
+		return answer;
+	}
 
 	public int solution(String myString, String pat) {
 		int answer = 0;
@@ -28,5 +45,8 @@ public class StringCounting {
 		String pat = "ana";
 		int solution1 = solution.solution(my_string, pat);
 		System.out.println("result = " + (solution1));
+
+		int solution2 = solution.solutionStream(my_string, pat);
+		System.out.println("result = " + (solution2));
 	}
 }
