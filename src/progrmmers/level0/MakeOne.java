9d16f5cd697f82dd80600b3c37acb31b845637cb
99 <hahahoho5915@gmail.com>
2023-06-16 18:22:46 +0900

    level0_1로 만들기_stream
    url: https://school.programmers.co.kr/learn/courses/30/lessons/181880

--- src/progrmmers/level0/MakeOne.java
+++ src/progrmmers/level0/MakeOne.java
@@ -1,5 +1,7 @@
 package progrmmers.level0;
 
+import java.util.Arrays;
+
 /*
 programmers
 level: 0
@@ -7,6 +9,19 @@ title: 1로 만들기
 url: https://school.programmers.co.kr/learn/courses/30/lessons/181880
 */
 public class MakeOne {
+
+	public int solutionStream(int[] num_list) {
+		return Arrays.stream(num_list)
+				.map(num -> {
+					int count = 0;
+					while (num != 1) {
+						num /= 2;
+						count++;
+					}
+					return count;
+				})
+				.sum();
+	}
 
 	public int solution(int[] num_list) {
 		int answer = 0;
@@ -25,5 +40,8 @@ public class MakeOne {
 		int[] arr = {12, 4, 15, 1, 14};
 		int solution1 = solution.solution(arr);
 		System.out.println("result = " + (solution1));
+
+		int solution2 = solution.solutionStream(arr);
+		System.out.println("result = " + (solution2));
 	}
 }
