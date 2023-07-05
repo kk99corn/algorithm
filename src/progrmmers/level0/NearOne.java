628e187fdc193c0d08d86ef90a45facec3c6f0a5
99 <hahahoho5915@gmail.com>
2023-07-05 18:49:45 +0900

    level0_가까운 1 찾기_stream
    url: https://school.programmers.co.kr/learn/courses/30/lessons/181898

--- src/progrmmers/level0/NearOne.java
+++ src/progrmmers/level0/NearOne.java
@@ -1,5 +1,7 @@
 package progrmmers.level0;
 
+import java.util.stream.IntStream;
+
 /*
 programmers
 level: 0
@@ -7,6 +9,13 @@ title: 가까운 1 찾기
 url: https://school.programmers.co.kr/learn/courses/30/lessons/181898
 */
 public class NearOne {
+
+	public int solutionStream(int[] arr, int idx) {
+		return IntStream.range(idx, arr.length)
+				.filter(i -> arr[i] == 1)
+				.findFirst()
+				.orElse(-1);
+	}
 
 	public int solution(int[] arr, int idx) {
 		int answer = -1;
@@ -26,5 +35,8 @@ public class NearOne {
 		int idx = 4;
 		int answer = solution.solution(arr, idx);
 		System.out.println("result = " + (answer));
+
+		int answer2 = solution.solutionStream(arr, idx);
+		System.out.println("result = " + (answer2));
 	}
 }
