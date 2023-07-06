4c61c9aa191f0bbd558e8e98b8117d43ce03cc0a
99 <hahahoho5915@gmail.com>
2023-07-06 18:50:22 +0900

    level0_문자열 잘라서 정렬하기_stream
    url: https://school.programmers.co.kr/learn/courses/30/lessons/181866

--- src/progrmmers/level0/SplitSort.java
+++ src/progrmmers/level0/SplitSort.java
@@ -3,6 +3,7 @@ package progrmmers.level0;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /*
 programmers
@@ -11,6 +12,18 @@ title: 문자열 잘라서 정렬하기
 url: https://school.programmers.co.kr/learn/courses/30/lessons/181866
 */
 public class SplitSort {
+
+	public String[] solutionStream(String myString) {
+		String[] list = myString.split("(x+)");
+		List<String> temp = Arrays.stream(list)
+				.filter(s -> !s.isBlank())
+				.collect(Collectors.toList());
+
+		String[] answer = temp.toArray(new String[0]);
+		Arrays.sort(answer);
+
+		return answer;
+	}
 
 	public String[] solution(String myString) {
 		String[] list = myString.split("(x+)");
@@ -31,5 +44,8 @@ public class SplitSort {
 		String myString = "dxccxbbbxaaaa";
 		String[] solution1 = solution.solution(myString);
 		System.out.println("result = " + Arrays.toString(solution1));
+
+		String[] solution2 = solution.solutionStream(myString);
+		System.out.println("result = " + Arrays.toString(solution2));
 	}
 }
