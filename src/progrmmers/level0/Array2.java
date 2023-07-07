22e2f976f78f62147b280ef9b6eb66d2fd2edde3
99 <hahahoho5915@gmail.com>
2023-07-07 18:36:20 +0900

    level0_배열의 길이를 2의 거듭제곱으로 만들기_stream
    url: https://school.programmers.co.kr/learn/courses/30/lessons/181857

--- src/progrmmers/level0/Array2.java
+++ src/progrmmers/level0/Array2.java
@@ -1,6 +1,7 @@
 package progrmmers.level0;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 /*
 programmers
@@ -9,6 +10,13 @@ title: 배열의 길이를 2의 거듭제곱으로 만들기
 url: https://school.programmers.co.kr/learn/courses/30/lessons/181857
 */
 public class Array2 {
+
+	public int[] solutionStream(int[] arr) {
+		int length = (int) Math.pow(2, (int) Math.ceil(Math.log(arr.length) / Math.log(2)));
+		return IntStream.range(0, length)
+				.map(i -> i < arr.length ? arr[i] : 0)
+				.toArray();
+	}
 
 	public int[] solution(int[] arr) {
 		int length = 1;
@@ -27,5 +35,8 @@ public class Array2 {
 		int[] arr = {1, 2, 3, 4, 5, 6};
 		int[] solution1 = solution.solution(arr);
 		System.out.println("result = " + Arrays.toString(solution1));
+
+		int[] solution2 = solution.solutionStream(arr);
+		System.out.println("result = " + Arrays.toString(solution2));
 	}
 }
