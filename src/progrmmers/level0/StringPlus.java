229fc135ef7585ac47ade81c3e131f0b87354cf0
99 <hahahoho5915@gmail.com>
2023-07-10 19:02:01 +0900

    level0_문자열 붙여서 출력하기
    url: https://school.programmers.co.kr/learn/courses/30/lessons/181946

---
+++ src/progrmmers/level0/StringPlus.java
@@ -0,0 +1,19 @@
+package progrmmers.level0;
+
+import java.util.Scanner;
+
+/*
+programmers
+level: 0
+title: 문자열 붙여서 출력하기
+url: https://school.programmers.co.kr/learn/courses/30/lessons/181946
+*/
+public class StringPlus {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		String b = sc.next();
+		System.out.print(a + b);
+	}
+}
