6baebf30f585f194b28843a7ba132eea4e8e3e48
99 <hahahoho5915@gmail.com>
2023-07-24 19:16:50 +0900

    level0_첫 번째로 나오는 음수_stream
    url: https://school.programmers.co.kr/learn/courses/30/lessons/181896

--- src/progrmmers/level0/FirstMinusNumber.java
+++ src/progrmmers/level0/FirstMinusNumber.java
@@ -1,5 +1,7 @@
 package progrmmers.level0;
 
+import java.util.stream.IntStream;
+
 /*
 programmers
 level: 0
@@ -7,6 +9,13 @@ title: 첫 번째로 나오는 음수
 url: https://school.programmers.co.kr/learn/courses/30/lessons/181896
 */
 public class FirstMinusNumber {
+
+	public int solutionStream(int[] num_list) {
+		return IntStream.range(0, num_list.length)
+				.filter(i -> num_list[i] < 0)
+				.findFirst()
+				.orElse(-1);
+	}
 
 	public int solution(int[] num_list) {
 		int answer = -1;
@@ -25,5 +34,8 @@ public class FirstMinusNumber {
 		int[] num_list = {12, 4, 15, 46, 38, -2, 15};
 		int result = solution.solution(num_list);
 		System.out.println("result = " + (result));
+
+		int result2 = solution.solutionStream(num_list);
+		System.out.println("result = " + (result2));
 	}
 }
