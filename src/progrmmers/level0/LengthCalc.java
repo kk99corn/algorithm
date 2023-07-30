f4d1198be07b5924dcb6bdaf6827bd903d65bc53
99 <hahahoho5915@gmail.com>
2023-07-30 23:19:08 +0900

    level0_길이에 따른 연산_stream
    url: https://school.programmers.co.kr/learn/courses/30/lessons/181879

--- src/progrmmers/level0/LengthCalc.java
+++ src/progrmmers/level0/LengthCalc.java
@@ -1,5 +1,7 @@
 package progrmmers.level0;
 
+import java.util.Arrays;
+
 /*
 programmers
 level: 0
@@ -7,6 +9,15 @@ title: 길이에 따른 연산
 url: https://school.programmers.co.kr/learn/courses/30/lessons/181879
 */
 public class LengthCalc {
+
+	public int solutionStream(int[] num_list) {
+		int answer = Arrays.stream(num_list).reduce((a, b) -> a * b).orElse(0);
+		if (num_list.length > 10) {
+			answer = Arrays.stream(num_list).sum();
+		}
+
+		return answer;
+	}
 
 	public int solution(int[] num_list) {
 		int answer = 0;
@@ -29,5 +40,8 @@ public class LengthCalc {
 		int[] num_list = {3, 4, 5, 2, 5, 4, 6, 7, 3, 7, 2, 2, 1};
 		int solution1 = solution.solution(num_list);
 		System.out.println("result = " + (solution1));
+
+		int solution2 = solution.solutionStream(num_list);
+		System.out.println("result = " + (solution2));
 	}
 }
