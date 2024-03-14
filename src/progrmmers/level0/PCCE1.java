db90e9f2254afc9cb230e0c6a3bb45f35e91e9c1
99 <hahahoho5915@gmail.com>
2024-03-14 11:02:49 +0900

    level0_[PCCE 기출문제] 1번 / 출력
    url: https://school.programmers.co.kr/learn/courses/30/lessons/250133

---
+++ src/progrmmers/level0/PCCE1.java
@@ -0,0 +1,20 @@
+package progrmmers.level0;
+
+/*
+programmers
+level: 0
+title: [PCCE 기출문제] 1번 / 출력
+url: https://school.programmers.co.kr/learn/courses/30/lessons/250133
+*/
+public class PCCE1 {
+
+	public static void main(String[] args) {
+		String msg = "Spring is beginning";
+		int val1 = 3;
+		String val2 = "3";
+
+		System.out.println(msg);
+		System.out.println(val1 + 10);
+		System.out.println(val2 + "10");
+	}
+}
