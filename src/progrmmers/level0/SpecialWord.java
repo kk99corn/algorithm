113150a9a7f0d8ebfc55277a893217505bfcbf8c
99 <hahahoho5915@gmail.com>
2024-04-26 20:31:41 +0900

    level0_특수문자 출력하기
    url: https://school.programmers.co.kr/learn/courses/30/lessons/181948

---
+++ src/progrmmers/level0/SpecialWord.java
@@ -0,0 +1,14 @@
+package progrmmers.level0;
+
+/*
+programmers
+level: 0
+title: 특수문자 출력하기
+url: https://school.programmers.co.kr/learn/courses/30/lessons/181948
+*/
+public class SpecialWord {
+
+	public static void main(String[] args) {
+		System.out.println("!@#$%^&*(\\'\"<>?:;");
+	}
+}
